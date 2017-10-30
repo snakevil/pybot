c403eca3cb739a085763606e0cca49f3283cc056
Snakevil Zen <zsnakevil@gmail.com>
2017-10-30 20:38:32 +0800

    定义图像模块像素点组件。

--- pybot/image/__init__.py
+++ pybot/image/__init__.py
@@ -11,11 +11,12 @@ class Color(object):
         self.b = b
 
     def __str__(self):
-        return '%d %d %d' % (self.r, self.g, self.b)
+        return '(%d %d %d)' % (self.r, self.g, self.b)
 
     def __eq__(self, another):
         r = g = b = -1
-        if type(another) == tuple:
+        its_type = type(another)
+        if its_type == tuple or its_type == list:
             r = another[0]
             g = another[1]
             b = another[2]
@@ -26,3 +27,35 @@ class Color(object):
                 b = another.b
             except: pass
         return self.r == r and self.g == g and self.b == b
+
+class Pixel(Color):
+    def __init__(self, pos, rgb):
+        self.x = pos[0]
+        self.y = pos[1]
+        super(Pixel, self).__init__(*rgb)
+
+    def __str__(self):
+        return '%d,%d %s' % (self.x, self.y, super(Pixel, self).__str__())
+
+    def __eq__(self, another):
+        x = y = r = g = b = -1
+        its_type = type(another)
+        try:
+            if its_type == tuple or its_type == list:
+                x = another[0][0]
+                y = another[0][1]
+                r = another[1][0]
+                g = another[1][1]
+                b = another[1][2]
+            else:
+                x = another.x
+                y = another.y
+                r = another.r
+                g = another.g
+                b = another.b
+        except: pass
+        return self.x == x and \
+            self.y == y and \
+            self.r == r and \
+            self.g == g and \
+            self.b == b
