5f5d543cc6a890c18e7f6059e5d976e3073251d0
Snakevil Zen <zsnakevil@gmail.com>
2017-10-30 21:54:34 +0800

    定义窗体包坐标区域组件。

--- pybot/player/__init__.py
+++ pybot/player/__init__.py
@@ -34,3 +34,57 @@ class Point(object):
             int(round(self.x + radius * math.cos(degree))),
             int(round(self.y - radius * math.sin(degree)))
         )
+
+class Rect(object):
+    def __init__(self, top_left, bottom_right = None):
+        left = top = right = bottom = 0
+        try:
+            if not bottom_right:
+                bottom_right = top_left[1]
+                top_left = top_left[0]
+            left = top_left[0]
+            top = top_left[1]
+            right = bottom_right[0]
+            bottom = bottom_right[1]
+        except: pass
+        self.left = min(left, right)
+        self.right = max(left, right)
+        self.top = min(top, bottom)
+        self.bottom = max(top, bottom)
+        self.width = self.right - self.left
+        self.height = self.bottom - self.top
+
+    def __str__(self):
+        return '%d,%d ~ %d,%d' % (self.left, self.top, self.right, self.bottom)
+
+    def __eq__(self, another):
+        top = right = bottom = left = -1
+        another_type = type(another)
+        try:
+            if another_type == tuple or another_type == list:
+                left = another[0][0]
+                top = another[0][1]
+                right = another[1][0]
+                bottom = another[1][1]
+            else:
+                left = another.left
+                top = another.top
+                right = another.right
+                bottom = another.bottom
+        except: pass
+        return self.left == left and \
+            self.top == top and \
+            self.right == right and \
+            self.bottom == bottom
+
+    def center(self):
+        return Point(
+            self.left + int(self.width / 2),
+            self.top + int(self.height / 2)
+        )
+
+    def random(self, padding = 0):
+        return Point(
+            random.randint(self.left + padding, self.right - padding),
+            random.randint(self.top + padding, self.bottom - padding)
+        )
