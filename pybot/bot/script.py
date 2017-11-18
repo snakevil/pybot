b6802b8f3ec17cb6d681da22f61b19c347ce27e1
Snakevil Zen <zsnakevil@gmail.com>
2017-11-18 17:25:31 +0800

    增加脚本模块的脚本组件对中断信号的捕捉和处理以优雅退出。

--- pybot/bot/script.py
+++ pybot/bot/script.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-import sys
+import signal
 from ..player import Window as Player
 from .expect import Base as Expect
 from .action import Base as Action
@@ -10,28 +10,42 @@ from .bot import Bot
 class Script(object):
     def __init__(self, *roles):
         self.roles = roles or ['']
-        self._target = roles[0]
+        self._target = self.roles[0]
         self._triggers = {role: [] for role in self.roles}
 
     def role(self, role = ''):
         assert role in self.roles
         self._target = role
+        return self
 
     def on(self, expect, action):
         assert isinstance(expect, Expect)
         assert isinstance(action, Action)
         self._triggers[self._target].append(Trigger(expect, action))
+        return self
+
+    def halt(self, signum, frame):
+        for role in self.roles:
+            self._bots[role].stop()
 
     def perform(self, players, **context):
         if isinstance(players, Player):
             players = {'': players}
-        bots = {}
+        self._bots = {}
+        signal.signal(signal.SIGINT, self.halt)
+        signal.signal(signal.SIGTERM, self.halt)
         for role in self.roles:
             assert isinstance(players.get(role), Player)
             players[role].aka(role or 'player')
-            context2 = {}
-            context2.update(context)
-            bots[role] = Bot(players[role], context2)
+            self._bots[role] = Bot(players[role], context.copy())
             for trigger in self._triggers[role]:
-                bots[role].handle(trigger)
-            bots[role].start()
+                self._bots[role].handle(trigger)
+            self._bots[role].start()
+        while True:
+            running = True
+            for role in self.roles:
+                running = running and self._bots[role].is_alive()
+            if running:
+                self._bots[self.roles[0]].join(.1)
+            else:
+                break
