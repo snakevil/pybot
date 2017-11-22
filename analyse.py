b395bd3e87d2890e49ccee73ba865a82e442de16
Snakevil Zen <zsnakevil@gmail.com>
2017-11-22 13:59:42 +0800

    增加全体窗口截屏工具组件。

--- analyse.py
+++ analyse.py
@@ -5,12 +5,15 @@ import pybot
 
 assert 5 < len(sys.argv)
 
-pic = pybot.image.load(
-    sys.argv[1]
-).crop(
+region = (
     (int(sys.argv[2]), int(sys.argv[3])),
     (int(sys.argv[4]), int(sys.argv[5]))
 )
+print('region: %r' % (region,))
+
+pic = pybot.image.load(
+    sys.argv[1]
+).crop(*region)
 print('histogram: %r' % (pic.histogram(1),))
 
 pic = pic.resize(8, 8).grayscale()
