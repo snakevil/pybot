dfe9ae02fbf9427f232406efc9f344efbbe9fb5e
Snakevil Zen <zsnakevil@gmail.com>
2017-12-01 11:10:27 +0800

    定义界面模块基于 Tcl/tk 的应用程序组件。

---
+++ pybot/gui/app.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+import tkinter as tk
+
+import sys
+from os import path
+
+__all__ = ['App']
+
+class App(object):
+    def __init__(self):
+        self.gui = tk.Tk()
+        self.on_close(self._on_close)
+        self.prefix = sys.prefix if hasattr(sys, 'frozen') \
+            else path.dirname(path.realpath(sys.argv[0]))
+
+    def _on_close(self):
+        self.gui.quit()
+
+    def run(self):
+        self.gui.mainloop()
+
+    def on_close(self, handler):
+        self.gui.protocol('WM_DELETE_WINDOW', handler)
