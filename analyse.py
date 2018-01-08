75143f97e223aee2b488d2b7e5f190c37382657a
Snakevil Zen <zsnakevil@gmail.com>
2018-01-08 18:49:02 +0800

    调整识别工具程序。

--- analyse.py
+++ analyse.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 # encoding: utf-8
 
 import sys
-import pybot
+from pybot import image
 
 assert 5 < len(sys.argv)
+
+threshold = 0 if 7 > len(sys.argv) else int(sys.argv[6])
 
 region = (
     (int(sys.argv[2]), int(sys.argv[3])),
@@ -11,11 +14,38 @@ region = (
 )
 print('region: %r' % (region,))
 
-pic = pybot.image.load(
+pic0 = image.load(
     sys.argv[1]
 ).crop(*region)
-print('histogram: %r' % (pic.histogram(1),))
+pic0.save('analyse-1-crop.png')
+print('histogram: %r' % (pic0.histogram(1),))
+pic = pic0.resize(8, 8).grayscale()
+print('fingerprint:')
+print('  digest: %r' % pic.binary(threshold).digest)
+print('  gray: %r' % pic.threshold)
 
-pic = pic.resize(8, 8).grayscale()
-print('threshold: %r' % pic.threshold)
-print('fingerprint: %r' % pic.binary().digest)
+pic = pic0.grayscale()
+pic.save('analyse-2-grayscale.png')
+print('grayscale:')
+print(
+    "  map: '''\n%s       '''" % (
+        '%s\n' % ('%02x' * pic.width) * pic.height % tuple(pic.rgba[0::4])
+    )
+)
+print(
+    "  template: '''\n%s\n            '''" % pic.dump()
+)
+
+pic = pic.binary()
+pic.save('analyse-2-binary.png')
+print('binary:')
+print(
+    "  map: '''\n%s       '''" % (
+        '%s\n' % ('%02x' * pic.width) * pic.height % tuple(
+            17 if i else 0 for i in pic.rgba[0::4]
+        )
+    )
+)
+print(
+    "  template: '''\n%s\n            '''" % pic.dump()
+)
