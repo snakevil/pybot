d618401cc744e7f5cb0ad342c631d9272bc85929
Snakevil Zen <zsnakevil@gmail.com>
2018-01-11 18:36:48 +0800

    优化脚本模块模板匹配预期组件的准确率。

--- pybot/bot/expect/template.py
+++ pybot/bot/expect/template.py
@@ -32,61 +32,75 @@ class Template(Expect):
         )
 
     def _test(self, event, trace):
-        x0 = y0 = 0
+        x = y = 0
         zone = event.screen
         if self._region:
-            x0 = self._region.left
-            y0 = self._region.top
+            x = self._region.left
+            y = self._region.top
             zone = event.screen.crop(
-                (x0, y0),
+                (x, y),
                 (self._region.right, self._region.bottom)
             )
         t0 = time.time()
-        x, y = self._search(zone.grayscale().binary(self._needle.threshold))
-        if 0 > x:
+        matched = self._search(zone.grayscale().binary(self._needle.threshold))
+        if not matched:
             return False
-        x += x0
-        y += y0
-        trace.append('= %r in %.3fms' % ((x, y), 1000 * (time.time() - t0)))
+        trace.append(
+            '%d matches in %.3f ms' % (
+                len(matched),
+                1000 * (time.time() - t0)
+            )
+        )
+        for point, distance in matched:
+            trace.append(
+                '= %8.3f/%8.3f (%d, %d)' % (
+                    distance,
+                    self._threshold,
+                    point[0] + x,
+                    point[1] + y
+                )
+            )
+        x += matched[0][0][0]
+        y += matched[0][0][1]
         self.spots = {
             k: (v[0].skew(x, y), v[1]) for k, v in self._spots.items()
         }
         return True
 
     def _search(self, haystack):
-        candidates = []
         width = haystack.width - self._needle.width + 1
-        for y in range(haystack.height - self._needle.height + 1):
-            candidates += [
-                (i, 0) for i \
-                    in range(haystack.width * y, haystack.width * y + width)
-            ]
-        threshold = self._threshold * self._needle.width // 100
+        candidates = [
+            (i, 0) \
+                for y in range(haystack.height - self._needle.height + 1) \
+                for i in range(haystack.width * y, haystack.width * y + width)
+        ]
         width = self._needle.width << 2
+        quota = self._threshold * self._needle.width / 100
         limit = 0
-        for y in range(self._needle.height):
-            limit += threshold
+        for y in range(self._needle.height -1, -1, -1):
+            limit += quota * y * 2 / (self._needle.height - 1)
             filtered = []
-            template = self._needle.rgba[width * y:width * (y + 1)]
+            nline = self._needle.rgba[width * y:width * (y + 1):4]
             for candidate, bad in candidates:
                 offset = (candidate + haystack.width * y) << 2
-                case = haystack.rgba[offset:offset + width]
-                if case == template:
-                    filtered.append((candidate, bad))
-                else:
-                    for i in range(0, width, 4):
-                        if case[i] != template[i]:
-                            bad += 1
-                        if bad > limit:
-                            break
-                    else:
-                        filtered.append((candidate, bad))
+                hline = haystack.rgba[offset:offset + width:4]
+                if hline != nline:
+                    bad += sum([1 for i, j in zip(hline, nline) if i != j])
+                    if bad > limit:
+                        continue
+                filtered.append((candidate, bad))
+            if not filtered:
+                return tuple()
             candidates = filtered
-            if not len(candidates):
-                break
-        if not candidates:
-            return (-1, -1)
-        return (
-            candidates[0][0] % haystack.width,
-            candidates[0][0] // haystack.width
+        size = self._needle.width * self._needle.height / 100
+        return tuple(
+            sorted(
+                [
+                    (
+                        (i % haystack.width, i // haystack.width),
+                        j / size
+                    ) for i, j in candidates
+                ],
+                key = lambda i: i[1]
+            )
         )
